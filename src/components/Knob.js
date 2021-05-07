71a01f60f6be6033f52b4ad1f2b8e347605566f3
Josh Frank <eurosewusa@gmail.com>
2021-05-07 14:07:19 -0400

    netifly build & toml, adding title & description to DOM

--- src/components/Knob.js
+++ src/components/Knob.js
@@ -1,24 +1,30 @@
 export default function Knob() {
 
-    return <svg
-        width="50px"
-        height="50px"
-        viewBox="0 0 100 100"
-        xmlns="http://www.w3.org/2000/svg"
+    return <div
+        className="knob-wrapper"
     >
-        <clipPath id="knob">
+        <input type="range" className="knob-input"></input>
+        <svg
+            className="knob-svg"
+            width="50px"
+            height="50px"
+            viewBox="0 0 100 100"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <clipPath id="knob">
+                <circle cx="50" cy="50" r="50" />
+            </clipPath>
             <circle cx="50" cy="50" r="50" />
-        </clipPath>
-        <circle cx="50" cy="50" r="50" />
-        <circle cx="50" cy="50" r="40" fill="rgb( 50, 50, 50 )" />
-        <line
-            clipPath="url( #knob )"
-            x1="50"
-            y1="0"
-            x2="50"
-            y2="50"
-            style={ { stroke: "rgb( 255, 255, 255 )", strokeWidth: 10 } }
-        />
-    </svg>;
+            <circle cx="50" cy="50" r="40" fill="rgb( 50, 50, 50 )" />
+            <line
+                clipPath="url( #knob )"
+                x1="50"
+                y1="0"
+                x2="50"
+                y2="50"
+                style={ { stroke: "rgb( 255, 255, 255 )", strokeWidth: 10 } }
+            />
+        </svg>
+    </div>;
 
 }
